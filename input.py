edac216ef5b2696c4a5414ba59eb53a69087299b
Dominik Imgrueth <dominik.imgrueth@gmail.com>
2023-01-09 23:14:30 +0100

    first real code implemented

--- input.py
+++ input.py
@@ -6,16 +6,41 @@ from typing import Dict
 input_dur = input('Hello dear bird friend! \nHow long would you like to count birds? \n\nAnswer in Minutes: ')
 try:
     duration = float(input_dur)
+    #TODO: check for negative numbers!
 except:
     print('I am sorry, this is not a number!')
     exit()
 
+#TODO: implement timer
 print("Time is running, let's watch out for birds!")
 
-birds = {'AMSEL': 0, 'KOHLMEISE': 0}
+#TODO: complete list with birdlife catalogue
+birds: dict[str, int] = {'AMSEL': 0,
+                         'KOHLMEISE': 0,
+                         'TANNENMEISE': 0,
+                         'BLAUMEISE': 0,
+                         'TAUBE': 0,
+                         'SPERLING': 0,
+                         'BUNTSPECHT': 0}
 
-bird = input('Which bird do you see?').upper()
-cnt = input('How many of them?')
+bird = input('Which bird do you see?\n').upper()
+#TODO: check if positive integer:
+cnt = int(input('How many of them?\n'))
 
-print("You saw ", cnt, bird)
+if bird not in birds.keys():
+    print(f'The bird {bird} is not yet in the catalogue. Do you want to add it [y]/n?')
+    if input().upper() == 'N':
+        print("Bird not added.")
+    else:
+        birds[bird] = cnt
+        print('Bird added to the catalogue.')
+else:
+    birds[bird] = cnt
+    print('Bird added to the catalogue.')
 
+
+#print("You saw ", cnt, bird)
+print('The result of today\'s count is:\n', birds)
+
+#TODO: add result to database including a date
+# if date exists, ask to overwrite
