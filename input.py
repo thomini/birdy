2da0e7e2e7f323b3f5a068bdb9ef1508f804273b
Dominik Imgrueth <dominik.imgrueth@gmail.com>
2023-01-10 00:10:32 +0100

    added timer

--- input.py
+++ input.py
@@ -1,5 +1,7 @@
 # this file represents the interface to the user when counting birds
 from typing import Dict
+import time
+import datetime
 
 # ask the user how long she would like to count birds
 
@@ -10,9 +12,6 @@ try:
 except:
     print('I am sorry, this is not a number!')
     exit()
-
-#TODO: implement timer
-print("Time is running, let's watch out for birds!")
 
 #TODO: complete list with birdlife catalogue
 birds: dict[str, int] = {'AMSEL': 0,
@@ -27,24 +26,36 @@ birds: dict[str, int] = {'AMSEL': 0,
                          'UHU': 0,
                          'STIEGLITZ': 0}
 
-bird = input('Which bird do you see?\n').upper()
-#TODO: check if positive integer:
-cnt = int(input('How many of them?\n'))
+# now implement the timer: add the duration to the time now and use it for an while loop
+time_quit = time.time() + duration * 60 # in seconds
+print("Time is running, let's watch out for birds!")
+while time_quit > time.time():
 
-if bird not in birds.keys():
-    print(f'The bird {bird} is not yet in the catalogue. Do you want to add it [y]/n?')
-    if input().upper() == 'N':
-        print("Bird not added.")
+    bird = input('Which bird do you see?\n').upper()
+    #TODO: check if positive integer:
+    try:
+        cnt = int(input('How many of them?\n'))
+        if cnt < 1:
+            print('Not a positive integer. Nothing added.')
+            continue
+    except:
+        print('Not an integer. Nothing added.')
+        continue
+
+    if bird not in birds.keys():
+        print(f'The bird {bird} is not yet in the catalogue. Do you want to add it [y]/n?')
+        if input().upper() == 'N':
+            print("Bird not added.")
+        else:
+            birds[bird] = cnt
+            print('Bird added to the catalogue.')
     else:
         birds[bird] = cnt
         print('Bird added to the catalogue.')
-else:
-    birds[bird] = cnt
-    print('Bird added to the catalogue.')
 
 
-#print("You saw ", cnt, bird)
-print('The result of today\'s count is:\n', birds)
+
+print('Time is up! \nThe result of today\'s count is:\n', birds)
 
 #TODO: add result to database including a date
 # if date exists, ask to overwrite
